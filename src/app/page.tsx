c8ed1c425a86136274dd91dd711d3757b28ed563
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-12 20:05:34 +0600

    learn SSG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 async function page() {
-  let response = await fetch('http://localhost:3000/api/user',{
-    cache:'no-store'
+
+  //  ============== SSR==============
+  // let response = await fetch('http://localhost:3000/api/user',{
+  //   cache:'no-store'
+  // });
+  // let data = await response.json()
+  // console.log(data);
+  //   =================       SSG   ==================
+    let response = await fetch('http://localhost:3000/api/user',{
+    cache:"force-cache"
   });
   let data = await response.json()
   console.log(data);
